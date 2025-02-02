0f212fe7b631e048d63111b74e4e35ebd1b7c3ed
thomasyap <thomasyap@moneymatch.co>
2025-02-02 14:32:25 +0800

    chore: added rates options into autocomplete component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,39 @@
 import { fetchRates } from "@/api/rates";
 import Calculator from "@/components/Calculator";
 import { List, Stack } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [allRates, setAllRates] = useState([]);
+
   const getRates = async () => {
     try {
-      const rates = await fetchRates();
-      console.log("  rate  ", rates);
-      return Response.json(rates);
+      const results = await fetchRates();
+      // const result = Response.json(rates);
+      const { success, quotes, source } = results || {};
+      if (success) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const quotesArr: any = [];
+
+        for (const [key, value] of Object.entries(quotes)) {
+          const cleanCurrency = key.replace(source, "");
+          quotesArr.push({
+            label: cleanCurrency,
+            currency: cleanCurrency,
+            rate: value,
+          });
+        }
+
+        setAllRates(quotesArr);
+      }
     } catch (err) {
       console.error("Main: ", err);
     }
   };
 
   useEffect(() => {
+    console.log("   main rendered");
     getRates();
   }, []);
 
@@ -25,7 +43,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Stack direction={{ md: "row", sx: "column" }} gap={3}>
-          <Calculator getRates={getRates} />
+          <Calculator allRates={allRates} />
           <List> Hey List</List>
         </Stack>
       </main>
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,12 +1,49 @@
-import { Stack } from "@mui/material";
-import Button from "@mui/material/Button";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Autocomplete, Stack, TextField } from "@mui/material";
+// import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 
-const Calculator = ({ getRates }) => {
+const Calculator = ({ allRates }) => {
+  const onItemChange = (value) => {
+    console.log("   onItemChangex  ", value);
+  };
+
   return (
     <Stack direction={"column"}>
       <Typography variant="h4">Rate calculator</Typography>
-      <Button onClick={getRates}>Get Rate</Button>
+
+      <Stack direction="column">
+        <Typography variant="h4">You Pay:</Typography>
+        <Autocomplete
+          disablePortal
+          // getOptionLabel={(option: any) => {
+          //   return option.label ? option.label.toString() : "";
+          // }}
+          options={allRates}
+          sx={{ width: 300 }}
+          // renderOption={(optionProp: any, option: any, optionState: any) => {
+          //   return (
+          //     <li {...optionProp}>
+          //       <Typography variant="subtitle1">{option.label}</Typography>
+          //     </li>
+          //   );
+          // }}
+          // value={inputValue}
+          onChange={(item, value) => {
+            onItemChange(value);
+          }}
+          isOptionEqualToValue={(option: any, curValue: any) =>
+            option?.value === curValue?.value
+          }
+          renderInput={(params) => {
+            return <TextField {...params} label="Currency" />;
+          }}
+        />
+      </Stack>
+
+      <Typography variant="h4">You Get:</Typography>
+
+      {/* <Button onClick={getRates}>Get Rate</Button> */}
     </Stack>
   );
 };
