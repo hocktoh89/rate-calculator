d52d330f198a01a9f594ea00d632b9130eb3690c
thomasyap <thomasyap@moneymatch.co>
2025-06-10 16:21:11 +0800

    rebase test 2nd commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 const ButtonA = () => {
   return (
     <Button color="primary" variant="outlined">
-      Hello Me
+      Hello Me I kacau here and there, added rebase 2nd commit
     </Button>
   );
 };
