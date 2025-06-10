8f20b6d1c43f3cbfa6714a07b00e6829653b7819
thomasyap <thomasyap@moneymatch.co>
2025-06-10 17:03:12 +0800

    rebase test commit 7 added Typography to NewFile2

--- src/components/NewFile2.tsx
+++ src/components/NewFile2.tsx
@@ -1,5 +1,11 @@
+import { Typography } from "@mui/material";
+
 const NewFile2 = () => {
-  return <>New File2</>;
+  return (
+    <>
+      New File<Typography>New Value</Typography>
+    </>
+  );
 };
 
 export default NewFile2;
