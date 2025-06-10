127f4d8752127f54e0ec7ebc3779bfc0213a995e
thomasyap <thomasyap@moneymatch.co>
2025-06-10 16:39:29 +0800

    rebase test commit 4 added more text in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,8 @@ import Button from "@mui/material/Button";
 const ButtonA = () => {
   return (
     <Button color="primary" variant="outlined">
-      Hello Me I kacau here and there, added rebase 2nd commit
+      Hello Me I kacau here and there, added rebase 2nd commit Hey, I am adding
+      more text here. rebase commit 4.
     </Button>
   );
 };
